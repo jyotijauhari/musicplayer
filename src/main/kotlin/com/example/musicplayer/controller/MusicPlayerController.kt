229f445c6acb47538ba9b44f26da889e15ed8960
jyotijauhari <jyotijauhari222@gmail.com>
2022-11-14 13:47:18 +0530

    Refactor. updated endpoint for fetching all songs from songs/all to songs/

--- src/main/kotlin/com/example/musicplayer/controller/MusicPlayerController.kt
+++ src/main/kotlin/com/example/musicplayer/controller/MusicPlayerController.kt
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("/songs")
 class MusicPlayerController(val service: MusicPlayerService) {
-    @GetMapping("/all")
+
+    @GetMapping
     fun getAllSongs() = service.getAll()
 
     @GetMapping("/{id}")
